8998b62c61a34f3cea860dd8d099a4000f609c1a
eXhumer <exhumer@exhumer.cc>
2025-01-27 19:51:26 +0600

    chore: better development check
    Signed-off-by: eXhumer <exhumer@exhumer.cc>

--- src/ink.tsx
+++ src/ink.tsx
@@ -92,7 +92,7 @@ export default class Ink {
 		// Unmount when process exits
 		this.unsubscribeExit = onExit(this.unmount, {alwaysLast: false});
 
-		if (process.env['DEV'] === 'true') {
+		if (process.env['NODE_ENV'] === 'development') {
 			reconciler.injectIntoDevTools({
 				bundleType: 0,
 				// Reporting React DOM's version, not Ink's
